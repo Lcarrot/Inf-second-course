6a787b04657f15e875a67733c125164b463b630d
Lcarrot <vip.kfkf2013@bk.ru>
2020-12-16 01:21:48 +0300

    swing is ready

--- FirstSemester/Swing/src/main/java/Main.java
+++ FirstSemester/Swing/src/main/java/Main.java
@@ -3,6 +3,8 @@ import javax.swing.border.BevelBorder;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -16,8 +18,11 @@ public class Main {
     private JRadioButtonMenuItem rbMenuItem;
     private JMenu submenu;
     private Shape shape;
-    ButtonGroup group;
-    private boolean status = true;
+    private ButtonGroup group;
+    private boolean isActiveTriangle = false;
+    private boolean isActiveForm = false;
+    private JTextField prompt;
+    private java.util.List<Component> componentList;
 
     private final Dimension dim = new Dimension(300, 300);
 
@@ -27,7 +32,7 @@ public class Main {
     }
 
     public void initGUI() {
-        frame = new JFrame("Window");
+        frame = new JFrame("Rotating square");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         //Create the menu bar.
@@ -43,11 +48,10 @@ public class Main {
         //a group of radio button menu items
         menu.addSeparator();
         group = new ButtonGroup();
-        rbMenuItem = new JRadioButtonMenuItem("Radio button menu item");
+        rbMenuItem = new JRadioButtonMenuItem("Radio button");
         rbMenuItem.setSelected(true);
-        addElement(rbMenuItem);
-        rbMenuItem = new JRadioButtonMenuItem("Another one");
-        addElement(rbMenuItem);
+        group.add(rbMenuItem);
+        menu.add(rbMenuItem);
 
         //group of check box menu items
         menu.addSeparator();
@@ -61,15 +65,6 @@ public class Main {
         submenu = new JMenu("Submenu");
         menuItem = new JMenuItem("Item in the submenu");
         submenu.add(menuItem);
-        menuItem = new JMenuItem("Another item");
-        submenu.add(menuItem);
-        menu.add(submenu);
-
-        //Exit menu item
-        menu.addSeparator();
-        menuItem = new JMenuItem("Exit");
-        menuItem.addActionListener(e -> System.exit(0));
-        menu.add(menuItem);
 
         //Build second menu in the menu bar.
         menu = new JMenu("About");
@@ -102,32 +97,86 @@ public class Main {
         JButton drawButton = new JButton("Draw square");
         rightPanel.add(drawButton);
         drawButton.addActionListener(e -> {
+            removeComponentsIfFlagTrue(isActiveForm);
+            panel.repaint();
             int width = panel.getSize().width;
             int height = panel.getSize().height;
-            shape = new Rectangle((width / 4), 30 + (height / 4), width / 2, height / 2);
-            graphics.setColor(Color.PINK);
-            graphics.fill(shape);
+            shape = new Rectangle((width / 3), (height / 3), width / 3, height / 3);
+            SwingUtilities.invokeLater(() -> {
+                graphics.draw(shape);
+                graphics.setColor(Color.BLACK);
+                graphics.fill(shape);
+            });
+        });
+        drawButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
 
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                prompt.setText("draw your figure");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                prompt.setText("");
+            }
         });
         JButton spinButton = new JButton("Spin Right Round");
+        spinButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                prompt.setText("spin your figure");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                prompt.setText("");
+            }
+        });
+        componentList = new LinkedList<>();
         rightPanel.add(spinButton);
         spinButton.addActionListener(e -> {
-            status = !status;
+            isActiveTriangle = !isActiveTriangle;
             new Thread(() -> {
-                while (true) {
-                    if (status) {
-                         
-                        break;
-                    }
+                while (isActiveTriangle) {
+                    removeComponentsIfFlagTrue(isActiveForm);
                     panel.repaint();
                     SwingUtilities.invokeLater(() -> {
-                        graphics.rotate(Math.toRadians(15), panel.getSize().width / 10, panel.getSize().height / 10);
+                        graphics.rotate(Math.toRadians(20), panel.getSize().width / 2.0, panel.getSize().height / 2.0);
                         graphics.draw(shape);
-                        graphics.setColor(Color.PINK);
+                        graphics.setColor(Color.BLACK);
                         graphics.fill(shape);
                     });
                     try {
-                        Thread.sleep(200);
+                        Thread.sleep(100);
                     } catch (InterruptedException interruptedException) {
                         interruptedException.printStackTrace();
                     }
@@ -135,6 +184,124 @@ public class Main {
             }).start();
         });
         JButton printButton = new JButton("Print Form");
+        //getForm
+        printButton.addActionListener(e -> {
+            isActiveTriangle = isActiveTriangle ? false : false;
+            isActiveForm = true;
+            panel.repaint();
+            JLabel lblEmailId = new JLabel("Email Id");
+            lblEmailId.setBounds(65, 115, 46, 14);
+            panel.add(lblEmailId);
+            componentList.add(lblEmailId);
+
+            JTextField textField_2 = new JTextField();
+            textField_2.setBounds(128, 112, 247, 17);
+            panel.add(textField_2);
+            textField_2.setColumns(10);
+            componentList.add(textField_2);
+
+            JLabel lblAddress = new JLabel("Address");
+            lblAddress.setBounds(65, 162, 46, 14);
+            panel.add(lblAddress);
+            componentList.add(lblAddress);
+
+            JTextArea textArea_1 = new JTextArea();
+            textArea_1.setBounds(126, 157, 212, 40);
+            panel.add(textArea_1);
+            textField_2.setColumns(10);
+            componentList.add(textArea_1);
+
+            JButton btnClear = new JButton("Clear");
+            btnClear.addActionListener(s -> {
+                textArea_1.setText("");
+                textField_2.setText("");
+            });
+            componentList.add(btnClear);
+
+            btnClear.setBounds(312, 387, 89, 23);
+            panel.add(btnClear);
+
+            JLabel lblSex = new JLabel("Sex");
+            lblSex.setBounds(65, 228, 46, 14);
+            panel.add(lblSex);
+            componentList.add(lblSex);
+
+            JLabel lblMale = new JLabel("Male");
+            lblMale.setBounds(128, 228, 46, 14);
+            panel.add(lblMale);
+            componentList.add(lblMale);
+
+            JLabel lblFemale = new JLabel("Female");
+            lblFemale.setBounds(292, 228, 46, 14);
+            panel.add(lblFemale);
+            componentList.add(lblFemale);
+
+            JRadioButton radioButton = new JRadioButton("");
+            radioButton.setBounds(337, 224, 109, 23);
+            panel.add(radioButton);
+            componentList.add(radioButton);
+
+            JRadioButton radioButton_1 = new JRadioButton("");
+            radioButton_1.setBounds(162, 224, 109, 23);
+            panel.add(radioButton_1);
+            componentList.add(radioButton_1);
+            JLabel lblOccupation = new JLabel("Occupation");
+            lblOccupation.setBounds(65, 288, 67, 14);
+            panel.add(lblOccupation);
+            componentList.add(lblOccupation);
+
+            JComboBox<String> comboBox = new JComboBox<>();
+            comboBox.addItem("Select");
+            comboBox.addItem("Business");
+            comboBox.addItem("Engineer");
+            comboBox.addItem("Doctor");
+            comboBox.addItem("Student");
+            comboBox.addItem("Others");
+            comboBox.addActionListener(event -> {
+            });
+            comboBox.setBounds(180, 285, 91, 20);
+            panel.add(comboBox);
+            componentList.add(comboBox);
+
+            JButton btnSubmit = new JButton("submit");
+            componentList.add(btnSubmit);
+            btnSubmit.addActionListener(p -> {
+                removeComponentsIfFlagTrue(isActiveForm);
+                isActiveForm = false;
+                panel.repaint();
+            });
+
+            btnSubmit.setBackground(Color.BLUE);
+            btnSubmit.setForeground(Color.MAGENTA);
+            btnSubmit.setBounds(65, 387, 89, 23);
+            panel.add(btnSubmit);
+        });
+        printButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                prompt.setText("OH MY");
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                prompt.setText("you can enter your data");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                prompt.setText("");
+            }
+        });
         rightPanel.add(printButton);
         rightPanel.setBackground(Color.LIGHT_GRAY);
         rightPanel.setPreferredSize(dim);
@@ -148,8 +315,11 @@ public class Main {
         //Build bottomPanel and status
         JPanel bottomPanel = new JPanel();
         bottomPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        prompt = new JTextField();
+        prompt.setEditable(false);
+        bottomPanel.add(prompt);
         frame.add(bottomPanel, BorderLayout.SOUTH);
-        bottomPanel.setPreferredSize(new Dimension(frame.getWidth(), 16));
+        bottomPanel.setPreferredSize(new Dimension(frame.getWidth(), 25));
         bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
 
         frame.setBounds(10, 10, 1900, 1000);
@@ -157,8 +327,12 @@ public class Main {
         graphics = (Graphics2D) frame.getGraphics();
     }
 
-    private <T extends JMenuItem> void addElement(T object) {
-        group.add(object);
-        menu.add(object);
+
+    private void removeComponentsIfFlagTrue(boolean flag) {
+        if (flag) {
+            for (Component component: componentList) {
+                panel.remove(component);
+            }
+        }
     }
 }
